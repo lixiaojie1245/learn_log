b28f99db8ace404679292c4f1ad80486cbebbd44
lxj <2819167124@qq.com>
2024-12-08 06:07:55 +0000

    modified

--- 241211_py_datetime/main.py
+++ 241211_py_datetime/main.py
@@ -1,9 +1,8 @@
-import os
 import datetime
-print(datetime.datetime.now().strftime('%y%m%d'))
+
+print(datetime.datetime.now().strftime("%y%m%d"))
 
 
 today = datetime.datetime.now()
 tomorrow = today + datetime.timedelta(days=1)
-print(tomorrow.strftime('%y%m%d'))
-
+print(tomorrow.strftime("%y%m%d"))
